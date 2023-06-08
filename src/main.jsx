12f4d34d9369ec164d90cb7e9e27965af6348a60
Anastasia Bushmakina <bushnastyaa@mail.ru>
2023-06-08 13:10:13 +0300

    Update Contacts with FormData

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import Root, {
 } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact, { action as editAction } from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,12 @@ const router = createBrowserRouter([
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ],
   },
